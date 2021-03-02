a02476296dd960b35850e28e014184d7c8a6aa05
rizalfakhri <me@rizalfakhri.id>
2021-03-03 03:34:35 +0800

    move to static asset

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 import tailwind from 'tailwind-rn';
 import { useResponsiveImageView } from 'react-native-responsive-image-view';
 import LogoRibbon from './components/LogoRibbon';
+import { videos } from './components/videos'
 
 import Svg, {
   Path
@@ -12,7 +13,139 @@ import Svg, {
 
 export default function App() {
 
-  const videos = require('./videos.json')
+
+  const videos = [
+      {
+          "id": 0,
+          "title": "ANTARA BENCANA ALAM DAN DARAH PERAWAN",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/ANTARA%20BENCANA%20ALAM%20DAN%20DARAH%20PERAWAN_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/bencana%20alam%20dan%20darah%20perwan.png",
+          "thumbnailStatic": require('./assets/images/0.png'),
+          "tags": [
+              "Menegangkan", "Mencekam", "Hantuuuuuu", "Wkwkwkwk"
+          ],
+          "categories": [
+              "mitos", "cerita-rakyat"
+          ]
+      },
+      {
+          "id": 1,
+          "title": "MUNCULNYA SATRIO PININGIT",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/MUNCULNYA%20SATRIO%20PININGIT_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/satrio%20piningit.png",
+          "thumbnailStatic": require('./assets/images/1.png'),
+          "tags": [
+              "Kisah Jawa", "Ramalan", "Anak Manusia"
+          ],
+          "categories": [
+              "ramalan", "kisah-jawa"
+          ]
+      },
+      {
+          "id": 2,
+          "title": "JIMAT POPOK WEWE",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/JIMAT%20POPOK%20WEWE_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/jimat-popok-wewe.png",
+          "thumbnailStatic": require('./assets/images/2.png'),
+          "tags": [
+              "Pesugihan", "Karma", "Kisah", "Kematian"
+          ],
+          "categories": [
+              "pesugihan", "kisah-nyata"
+          ]
+      },
+      {
+          "id": 3,
+          "title": "DARAH KORBAN KECELAKAAN",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/DARAH%20KORBAN%20KECELAKAAN_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/DARAH%20KORBAN%20KECELAKAAN.png",
+          "thumbnailStatic": require('./assets/images/3.png'),
+          "tags": [
+              "Menegangkan", "Kecelakaan", "Mitos"
+          ],
+          "categories": [
+              "mitos"
+          ]
+      },
+      {
+          "id": 4,
+          "title": "DENDAM ROH TABRAK LARI",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/DENDAM%20ROH%20TABRAK%20LARI_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/DENDAM%20ROH%20TABRAK%20LARI.png",
+          "thumbnailStatic": require('./assets/images/4.png'),
+          "tags": [
+              "Menegangkan", "Mitos", "Setelah Mati", "Roh"
+          ],
+          "categories": [
+              "mitos"
+          ]
+      },
+      {
+          "id": 5,
+          "title": "SIMBAH PERNAH MENGALAMI SAKARATUL MAUT",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/SIMBAH%20PERNAH%20MENGALAMI%20SAKARATUL%20MAUT%20Proses%20Keluarnya%20Roh%20dari%20Jasad%20PART%202_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/SAKARATUL%20MAUT%20PT1.png",
+          "thumbnailStatic": require('./assets/images/5.png'),
+          "tags": [
+              "Kematian", "Menegangkan", "Kisah Simbah", "Cerita"
+          ],
+          "categories": [
+              "kisah-nyata", "simbah"
+          ]
+      },
+      {
+          "id": 6,
+          "title": "SIMBAH vs. HANTU GUNUNG CIREMAI",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/SIMBAH%20vs%20Hantu%20GUNUNG%20CIREMAI_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/simbah%20vs%20gunung%20ciremai,%20eh%20hantu.png",
+          "thumbnailStatic": require('./assets/images/6.png'),
+          "tags": [
+              "Cerita Simbah", "Pertarungan Sengit", "Hantuuuuuu", "Wkwkwkwk"
+          ],
+          "categories": [
+              "kisah-nyata", "simbah"
+          ]
+      },
+      {
+          "id": 7,
+          "title": "MENCARI ANAK HILANG DENGAN NYANYIAN TAMPAH",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/MENCARI%20ANAK%20HILANG%20dengan%20NYANYIAN%20TAMPAH_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/anak%20hilang%20vs%20tampah%20aowkaowka.png",
+          "thumbnailStatic": require('./assets/images/7.png'),
+          "tags": [
+              "Kisah Rakyat", "Mencekam", "Hantu", "Gak tau apalagi wkwkw"
+          ],
+          "categories": [
+              "mitor", "cerita-rakyat"
+          ]
+      },
+      {
+          "id": 8,
+          "title": "TEROR ARWAH JANIN DI KOST WANITA",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/TEROR%20ARWAH%20JANIN%20DI%20KOST%20WANITA_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/TEROR%20ARWAH%20JANIN%20KOST%20WANITA.png",
+          "thumbnailStatic": require('./assets/images/8.png'),
+          "tags": [
+              "Teror Ghaib", "Mencekam", "Hantu", "Pengalaman"
+          ],
+          "categories": [
+              "ghaib", "roh"
+          ]
+      },
+      {
+          "id": 9,
+          "title": "PERJALANAN MANUSIA DARI SEBELUM LAHIR HINGGA KEMATIAN",
+          "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/PERJALANAN%20MANUSIA%20DARI%20SEBELUM%20LAHIR%20HINGGA%20KEMATIANNYA_1080p.mp4",
+          "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/PERJALANAN%20MANUSIA%20DARI%20SEBELUM%20LAHR%20HINGGA%20KEMATIAN.png",
+          "thumbnailStatic": require('./assets/images/8.png'),
+          "tags": [
+              "Kematian", "Setelah Mati", "Cerita", "Perjalananananananan"
+          ],
+          "categories": [
+              "kisah-jawa"
+          ]
+      }
+  ];
 
   const [ recomendedVideo, setRecomendedVideo ] = useState(videos[0])
 
@@ -31,7 +164,7 @@ export default function App() {
     return () => clearInterval(videoInterval)
   }, [])
 
-  const { getViewProps, getImageProps } = useResponsiveImageView({ source: { uri: recomendedVideo.thumbnail} });
+  const { getViewProps, getImageProps } = useResponsiveImageView({ source: recomendedVideo.thumbnailStatic });
 
   const recomendedVideoThumbnailHeight = (windowHeight > 800) ? hp('70%') : hp('80%')
 
@@ -51,7 +184,7 @@ export default function App() {
 
   const renderWatchedVideoCard = ({ item }) => {
 
-    const { title, thumbnail } = item
+    const { thumbnailStatic } = item
 
 
     return (
@@ -60,7 +193,7 @@ export default function App() {
           <Image source={LogoRibbon()} style={tailwind('h-12')}/>
         </View>
 
-        <ImageBackground source={{ uri: thumbnail }} style={tailwind('h-40 w-28 relative')}>
+        <ImageBackground source={thumbnailStatic} style={tailwind('h-40 w-28 relative')}>
           <Svg style={[tailwind('w-16 h-16 absolute  inset-7')]}  viewBox="0 0 95 95" fill="none" xmlns="http://www.w3.org/2000/svg">
             <Path d="M47.5 87.0833C69.3613 87.0833 87.0834 69.3613 87.0834 47.5C87.0834 25.6387 69.3613 7.91666 47.5 7.91666C25.6387 7.91666 7.91669 25.6387 7.91669 47.5C7.91669 69.3613 25.6387 87.0833 47.5 87.0833Z" fill-opacity="0.1" stroke="white" stroke-opacity="0.9" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
             <Path d="M39.5833 31.6667L63.3333 47.5L39.5833 63.3333V31.6667Z" stroke="white" stroke-opacity="0.9" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -78,7 +211,7 @@ export default function App() {
 
   const renderVideoCard = ({ item }) => {
 
-    const { title, thumbnail } = item
+    const { thumbnailStatic } = item
 
     return (
       <View style={tailwind('flex relative')}>
@@ -86,7 +219,8 @@ export default function App() {
           <Image source={LogoRibbon()} style={tailwind('h-12')}/>
         </View>
 
-        <ImageBackground source={{ uri: thumbnail }} style={tailwind('h-40 w-28 relative')} />
+
+        <ImageBackground source={thumbnailStatic} style={tailwind('h-40 w-28 relative')} />
 
       </View>
     )
@@ -95,8 +229,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <ScrollView contentContainerStyle={{flexGrow: 1, backgroundColor: '#181818'}}>
-        <View style={{  alignItems: 'center', position: 'relative', marginBottom: windowHeight + recomendedVideoThumbnailHeight }}>
+      <ScrollView contentContainerStyle={{flexGrow: 1, backgroundColor: '#181818'}} style={{ backgroundColor: '#181818' }}>
+        <View style={{  alignItems: 'center', position: 'relative', marginBottom: windowHeight + (20/100 * recomendedVideoThumbnailHeight) }}>
           <View style={[  styles.recomendedVideo, { height: recomendedVideoThumbnailHeight }  ]}>
 
             <View style={[tailwind('absolute inset-0 ml-4 w-10 z-20')]}>
@@ -188,11 +322,6 @@ export default function App() {
 
         </View>
 
-        <View style={tailwind('absolute bottom-0 mb-10 h-10 w-96')}>
-
-          <Text style={tailwind('text-center font-bold text-4xl px-5 text-white')}>ABIS GAN 😎</Text>
-
-        </View>
       </ScrollView>
 
       <View style={[tailwind('bottom-0 border-t border-white border-opacity-30 '), { backgroundColor: '#202020', position: 'fixed', height: '7%' }]}>
--- components/videos.js
+++ components/videos.js
@@ -1,10 +1,10 @@
-[
+const videos = [
     {
         "id": 0,
         "title": "ANTARA BENCANA ALAM DAN DARAH PERAWAN",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/ANTARA%20BENCANA%20ALAM%20DAN%20DARAH%20PERAWAN_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/bencana%20alam%20dan%20darah%20perwan.png",
-        "thumbnailStatic": "/assets/images/0.png",
+        "thumbnailStatic": require('../assets/images/0.png'),
         "tags": [
             "Menegangkan", "Mencekam", "Hantuuuuuu", "Wkwkwkwk"
         ],
@@ -17,7 +17,7 @@
         "title": "MUNCULNYA SATRIO PININGIT",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/MUNCULNYA%20SATRIO%20PININGIT_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/satrio%20piningit.png",
-        "thumbnailStatic": "/assets/images/1.png",
+        "thumbnailStatic": require('../assets/images/1.png'),
         "tags": [
             "Kisah Jawa", "Ramalan", "Anak Manusia"
         ],
@@ -25,14 +25,12 @@
             "ramalan", "kisah-jawa"
         ]
     },
-
     {
         "id": 2,
         "title": "JIMAT POPOK WEWE",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/JIMAT%20POPOK%20WEWE_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/jimat-popok-wewe.png",
-        "thumbnailStatic": "/assets/images/2.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/2.png'),
         "tags": [
             "Pesugihan", "Karma", "Kisah", "Kematian"
         ],
@@ -45,8 +43,7 @@
         "title": "DARAH KORBAN KECELAKAAN",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/DARAH%20KORBAN%20KECELAKAAN_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/DARAH%20KORBAN%20KECELAKAAN.png",
-        "thumbnailStatic": "/assets/images/3.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/3.png'),
         "tags": [
             "Menegangkan", "Kecelakaan", "Mitos"
         ],
@@ -59,8 +56,7 @@
         "title": "DENDAM ROH TABRAK LARI",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/DENDAM%20ROH%20TABRAK%20LARI_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/DENDAM%20ROH%20TABRAK%20LARI.png",
-        "thumbnailStatic": "/assets/images/4.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/4.png'),
         "tags": [
             "Menegangkan", "Mitos", "Setelah Mati", "Roh"
         ],
@@ -73,8 +69,7 @@
         "title": "SIMBAH PERNAH MENGALAMI SAKARATUL MAUT",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/SIMBAH%20PERNAH%20MENGALAMI%20SAKARATUL%20MAUT%20Proses%20Keluarnya%20Roh%20dari%20Jasad%20PART%202_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/SAKARATUL%20MAUT%20PT1.png",
-        "thumbnailStatic": "/assets/images/5.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/5.png'),
         "tags": [
             "Kematian", "Menegangkan", "Kisah Simbah", "Cerita"
         ],
@@ -87,8 +82,7 @@
         "title": "SIMBAH vs. HANTU GUNUNG CIREMAI",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/SIMBAH%20vs%20Hantu%20GUNUNG%20CIREMAI_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/simbah%20vs%20gunung%20ciremai,%20eh%20hantu.png",
-        "thumbnailStatic": "/assets/images/6.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/6.png'),
         "tags": [
             "Cerita Simbah", "Pertarungan Sengit", "Hantuuuuuu", "Wkwkwkwk"
         ],
@@ -101,8 +95,7 @@
         "title": "MENCARI ANAK HILANG DENGAN NYANYIAN TAMPAH",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/MENCARI%20ANAK%20HILANG%20dengan%20NYANYIAN%20TAMPAH_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/anak%20hilang%20vs%20tampah%20aowkaowka.png",
-        "thumbnailStatic": "/assets/images/7.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/7.png'),
         "tags": [
             "Kisah Rakyat", "Mencekam", "Hantu", "Gak tau apalagi wkwkw"
         ],
@@ -115,8 +108,7 @@
         "title": "TEROR ARWAH JANIN DI KOST WANITA",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/TEROR%20ARWAH%20JANIN%20DI%20KOST%20WANITA_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/TEROR%20ARWAH%20JANIN%20KOST%20WANITA.png",
-        "thumbnailStatic": "/assets/images/8.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/8.png'),
         "tags": [
             "Teror Ghaib", "Mencekam", "Hantu", "Pengalaman"
         ],
@@ -129,8 +121,7 @@
         "title": "PERJALANAN MANUSIA DARI SEBELUM LAHIR HINGGA KEMATIAN",
         "video_url": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/PERJALANAN%20MANUSIA%20DARI%20SEBELUM%20LAHIR%20HINGGA%20KEMATIANNYA_1080p.mp4",
         "thumbnail": "https://kodinger-cdn.sgp1.cdn.digitaloceanspaces.com/algoritma-alam/thumbnails/with-watermark/PERJALANAN%20MANUSIA%20DARI%20SEBELUM%20LAHR%20HINGGA%20KEMATIAN.png",
-        "thumbnailStatic": "/assets/images/9.png",
-        "tags": [
+        "thumbnailStatic": require('../assets/images/8.png'),
         "tags": [
             "Kematian", "Setelah Mati", "Cerita", "Perjalananananananan"
         ],
@@ -138,4 +129,5 @@
             "kisah-jawa"
         ]
     }
-]
+];
+
